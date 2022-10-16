18fc87bf4f5e6725c802cc4369da821b1ff1f2ea
yalh76 <yalh@yahoo.com>
2022-10-16 22:06:33 +0200

    Apply last example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,11 +3,16 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+# PHP APP SPECIFIC
+#=================================================
 
 YNH_PHP_VERSION="8.0"
 
-# dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd"
+php_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd"
+
+# dependencies used by the app (must be on a single line)
+pkg_dependencies="$php_dependencies"
+
 pg_pkg_dependencies="postgresql postgresql-contrib"
 
 #=================================================
