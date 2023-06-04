4df62cb788901dfaeb3d4881a4bba8f0d067fa31
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-04 11:57:04 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,15 +6,6 @@
 # PHP APP SPECIFIC
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="8.2"
-
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-json"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="$php_dependencies"
-
-#REMOVEME? pg_pkg_dependencies="postgresql postgresql-contrib"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
