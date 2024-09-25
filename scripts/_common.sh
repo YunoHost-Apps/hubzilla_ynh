e92381eea201d0972602e925dece0a67a9e2f826
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-09-25 16:31:34 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,13 +3,11 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
-# PHP APP SPECIFIC
-#=================================================
-YNH_PHP_VERSION="8.2"
+
 mysql_remove() {
-	# FIXMEhelpers2.1 ynh_mysql_drop_db && ynh_mysql_drop_user --db_user=$db_user --db_name=$db_name
+	ynh_mysql_drop_db && ynh_mysql_drop_user --db_user=$db_user --db_name=$db_name
 }
 
 mysql_restore() {
-	# FIXMEhelpers2.1 ynh_mysql_create_db --db_user=$db_user --db_name=$db_name --db_pwd=$db_pwd
+	ynh_mysql_create_db --db_user=$db_user --db_name=$db_name --db_pwd=$db_pwd
 }
